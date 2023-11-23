a6dad9e191d8a745df3acd37770d11923c41a2b9
LucasCruz <lca.snf21@uea.edu.br>
2023-11-23 10:45:20 -0400

    feat: create GameInput script

---
+++ Assets/_Scripts/GameInput.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInput : MonoBehaviour
+{
+    private static GameInput instance;
+    public static GameInput Instance { get{ return instance;} }
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }else if(instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public Vector2 InputAxisNormalized()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        return new Vector2(x, y).normalized;
+    }
+}
