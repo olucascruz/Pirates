4b887e2a66d3d04ea5e3b056663b80f8216b5e1d
LucasCruz <lca.snf21@uea.edu.br>
2023-11-26 22:58:00 -0400

    feat: Add UIController

---
+++ Assets/_Scripts/UIController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI timerTMP;
+    [SerializeField] private GameObject canvasGameOver;
+    [SerializeField] private TextMeshProUGUI feedbackTMP;
+    [SerializeField] private TextMeshProUGUI pointsTMP;
+
+    [TextArea]
+    [SerializeField] private string positiveFeedbackText;
+
+    [TextArea]
+    [SerializeField] private string negativeFeedbackText;
+
+
+
+    private static UIController instance;
+    public static UIController Instance { get { return instance; } }
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        timerTMP.text = $"0";
+    }
+
+   
+    public void UpdateTimerTMP(int seconds)
+    {
+        int minutes = Mathf.FloorToInt(seconds / 60);
+        int remainingSeconds = Mathf.FloorToInt(seconds % 60);
+        string formattedTime = string.Format("{0:00}:{1:00}", minutes, remainingSeconds);
+
+        timerTMP.text = formattedTime;
+    }
+
+    public void GameOverUI(bool timesUp, int points)
+    {
+        canvasGameOver.SetActive(true);
+        if (timesUp) feedbackTMP.text = positiveFeedbackText;
+        else feedbackTMP.text = negativeFeedbackText;
+        pointsTMP.text = $"Points: {points}";
+
+    }
+}
