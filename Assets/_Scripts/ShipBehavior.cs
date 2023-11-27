d33a6f7cf74af2223a514f39574cb939f6aa1006
LucasCruz <lca.snf21@uea.edu.br>
2023-11-26 22:51:52 -0400

    remove: Delete unsual scripts

--- Assets/_Scripts/ShipBehavior.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-
-public abstract class ShipBehavior : ScriptableObject
-{
-    public abstract void Behavior(ShipController shipController);
-}
