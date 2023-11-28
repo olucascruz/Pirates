d5f7ed25e3cc54be23c381f4baa181b0d7de2219
LucasCruz <lca.snf21@uea.edu.br>
2023-11-27 22:33:53 -0400

    refactor: Add comments

--- Assets/_Scripts/GameRules/GenerateEnemies.cs
+++ Assets/_Scripts/GameRules/GenerateEnemies.cs
@@ -6,26 +6,37 @@ public class GenerateEnemies : MonoBehaviour
     [SerializeField] private Transform[] pointsSpawn;
     [SerializeField] private PoolingHandler poolShipChaser;
     [SerializeField] private PoolingHandler poolShipShooter;
-
-
+    [Header("scriptable object to define in section X seconds to Enemy Spawns")]
+    [SerializeField] private GameOptions gameOptions;
 
     void Start()
     {
         StartCoroutine(Generate());
     }
 
-    
+    /// <summary>
+    /// Spawns enemies in the scene
+    /// </summary>
     private IEnumerator Generate()
     {
-        while (GameManager.Instance.State == StateGame.PLAY) { 
-            if(poolShipShooter.IsOk && poolShipChaser.IsOk) { 
-                yield return new WaitForSeconds(10f);
+        //Wait while the game is in the starting state
+        while (GameManager.Instance.State == GameState.STARTING)
+        {
+           yield return new WaitForFixedUpdate();
+        }
+        while (GameManager.Instance.State == GameState.PLAY) {
+            //Check if enemy pools are full
+            if (poolShipShooter.IsFilled && poolShipChaser.IsFilled) { 
+                yield return new WaitForSeconds(gameOptions.timeToSpawnEnemies);
                 for (int i = 0; i < 2; i++)
                 {
                     GameObject[] enemy = { poolShipShooter.GetPooledObject(),
                                            poolShipChaser.GetPooledObject() };
 
+                    //One of the four spawn locations is chosen at random
                     int pointSpawnIndex = Random.Range(0, 4);
+
+                    //The type of enemy generated is chosen randomly
                     int typeEnemyIndex = Random.Range(0, 2);
 
                     GameObject enemyObj = enemy[typeEnemyIndex];
